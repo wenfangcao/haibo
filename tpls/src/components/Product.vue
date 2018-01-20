389e8b22f174b98deaa69b575d837e108339693d
web <>
2018-01-20 17:10:57 +0800

    1/20

--- tpls/src/components/Product.vue
+++ tpls/src/components/Product.vue
@@ -1,22 +1,36 @@
 <template>
-  <div>
+  <div class="pro">
       <Header class="headerdiv"></Header>
       <div class="bantitle text-center">
-		<h1>Product</h1>
-		<h5 class="btn">CLICK ENTER</h5>
-		<span>HAIB Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, voluptatum, ea, consequatur corporis <br> dolores ex maiores a tempora eligendi iste sapiente mollitia inventore aut rem aperiam totam aliquid laudantium tempore.</span>
-	</div>
-	<div class="proNav">
-		<div><a>首页</a></div>
-		<div><a>产品中心</a></div>
-		<div><a>2017 A/W</a></div>                                     
-	</div>
-	<div>
-		<div class="btn proMinTitle">系列产品</div>
-		<div><a>2017 A/W</a></div>
-		<div><a>2017 s/s</a></div>
-		<div><a>2017 a/w</a></div>
-	</div>
+			<h1>Product</h1>
+			<h5 class="btn">CLICK ENTER</h5>
+			<span>HAIB Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, voluptatum, ea, consequatur corporis <br> dolores ex maiores a tempora eligendi iste sapiente mollitia inventore aut rem aperiam totam aliquid laudantium tempore.</span>
+		</div>
+		<div class="proNav container">
+			<div class="row nav-no1">
+				<div class="col-xs-4"><a>首页</a></div>
+				<div class="col-xs-4"><a>产品中心</a></div>
+				<div class="col-xs-4"><a>2017 A/W</a></div>  
+			</div>                                
+		</div>
+		<div class="container margin20 minNav">
+			<div class="row">
+				<div class="col-xs-6">
+					<div class="btn proMinTitle">系列产品</div>
+				</div>
+				<div class="col-xs-6">
+						<div class="row minNavRight">
+							<div class="col-xs-4"><a>2017 A/W</a></div>
+							<div class="col-xs-4"><a>2017 s/s</a></div>
+							<div class="col-xs-4"><a>2017 a/w</a></div>
+						</div>		
+				</div>
+			</div>
+		</div>
+
+		<div>
+			
+		</div>
   </div>
 </template>
 <style>
